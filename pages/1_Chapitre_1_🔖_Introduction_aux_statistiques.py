9e8352b36488086e163ee535238c064b3cded299
ChrisMuba <63189970+ChrisMuba@users.noreply.github.com>
2023-09-07 17:15:49 +0200

    Update 1_Chapitre_1_🔖_Introduction_aux_statistiques.py

--- pages/1_Chapitre_1_🔖_Introduction_aux_statistiques.py
+++ pages/1_Chapitre_1_🔖_Introduction_aux_statistiques.py
@@ -339,8 +339,6 @@ if st.button("Continuer vers la suite du Chap.1 - **C/ Rôle des statistiques da
 
     st.markdown("")
 
-    
-
     st.markdown("Ci-dessous un exemple de pyramide des âges en **pelote de laine**")
 
     import streamlit as st
@@ -389,7 +387,7 @@ if st.button("Continuer vers la suite du Chap.1 - **C/ Rôle des statistiques da
 ]
 
     fig = go.Figure(data=data, layout=layout)
-
+    st.markdown("Ci-dessous un exemple de pyramide des âges en **pelote de laine**")
     st.plotly_chart(fig)
 
 
