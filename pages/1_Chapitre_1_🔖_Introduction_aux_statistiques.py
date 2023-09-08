fcd93a82d58e75a57d3cf646bd70e7cb727cb8fa
ChrisMuba <63189970+ChrisMuba@users.noreply.github.com>
2023-09-08 23:37:15 +0200

    Update 1_Chapitre_1_🔖_Introduction_aux_statistiques.py

--- pages/1_Chapitre_1_🔖_Introduction_aux_statistiques.py
+++ pages/1_Chapitre_1_🔖_Introduction_aux_statistiques.py
@@ -500,7 +500,21 @@ if st.button("Continuer vers la suite du Chap.1 - **C/ Rôle des statistiques da
 
     link = '[GitHub](http://github.com)'
     st.markdown(link, unsafe_allow_html=True)
+    
 
+    #st.markdown("**Une solide compréhension des statistiques va donc aider les professionnels RH à prendre des décisions impactantes fondées sur des données**.")
+
+
+
+    from bokeh.models.widgets import Div
+    import streamlit as st
+
+    if st.button('Go to Streamlit'):
+        js = "window.open('https://www.streamlit.io/')"  # New tab or window
+        js = "window.location.href = 'https://www.streamlit.io/'"  # Current tab
+        html = '<img src onerror="{}">'.format(js)
+        div = Div(text=html)
+        st.bokeh_chart(div)
 
 
                 
