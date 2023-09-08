d776c6ddc49efdd026486ba3c2dbb7aba01c3a3f
ChrisMuba <63189970+ChrisMuba@users.noreply.github.com>
2023-09-08 23:24:37 +0200

    Update 1_Chapitre_1_🔖_Introduction_aux_statistiques.py

--- pages/1_Chapitre_1_🔖_Introduction_aux_statistiques.py
+++ pages/1_Chapitre_1_🔖_Introduction_aux_statistiques.py
@@ -494,8 +494,6 @@ if st.button("Continuer vers la suite du Chap.1 - **C/ Rôle des statistiques da
     url = "https://cours-stats-rh.streamlit.app/Quiz_1_-_Introduction_aux_Statistiques📉"
     st.write("Acceder au quiz du chapitre 1 [ici](%s)" % url)
 
-    st.markdown("Aller directement au chap.2  [ici](%s)" % url)
-
 
                 
     #st.markdown("**Une solide compréhension des statistiques va donc aider les professionnels RH à prendre des décisions impactantes fondées sur des données**.")
