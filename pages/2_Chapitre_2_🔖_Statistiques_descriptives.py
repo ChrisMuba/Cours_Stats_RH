237c42d18db25d4b333ee43d54c01356ba4ab3ab
ChrisMuba <63189970+ChrisMuba@users.noreply.github.com>
2023-09-13 15:52:14 +0200

    Update 2_Chapitre_2_🔖_Statistiques_descriptives.py

--- pages/2_Chapitre_2_🔖_Statistiques_descriptives.py
+++ pages/2_Chapitre_2_🔖_Statistiques_descriptives.py
@@ -391,9 +391,9 @@ if st.button("Continuer vers la suite du Chap.2 - **B/ Mesures de la variabilit�
     
     st.markdown("- **Variance** : La variance est une mesure de variabilité plus sophistiquée que la plage. C'est une mesure de **comment** les données d'un ensemble diffèrent de la moyenne de l'ensemble.") 
 
-    # Display the theoretical formula for calculating the mean
+    # Display the theoretical formula for calculating the variance
     st.subheader("Formule théorique de la variance")
-    st.latex(r'\text{Moyenne} = \frac{\sum_{i=1}^{n}X_i}{n}')
+    st.latex(r'\text{Variance} = \frac{\sum_{i=1}^{n}X_i}{n}')
                 
     st.markdown("- **Écart type** : L'écart type est une mesure de la dispersion des valeurs des données par rapport à la moyenne de l'ensemble de données. C'est la racine carrée de la variance. Il s'agit de la mesure de variabilité la plus couramment utilisée et fournit une mesure de la dispersion dans les mêmes unités que l'ensemble de données d'origine.")            
 
