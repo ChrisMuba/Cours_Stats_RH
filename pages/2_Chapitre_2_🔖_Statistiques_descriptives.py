c821dd0e4112db07f4d972fbdee8d5f0f721dffd
ChrisMuba <63189970+ChrisMuba@users.noreply.github.com>
2023-09-13 20:47:11 +0200

    Update 2_Chapitre_2_🔖_Statistiques_descriptives.py

--- pages/2_Chapitre_2_🔖_Statistiques_descriptives.py
+++ pages/2_Chapitre_2_🔖_Statistiques_descriptives.py
@@ -430,6 +430,19 @@ if st.button("Continuer vers la suite du Chap.2 - **B/ Mesures de la variabilit�
     # Display the dataset
     st.write("Voici leurs scores de satisfaction :")
     st.write(df)
+
+    st.markdown("")
+    
+
+    st.markdown("Maintenant, calculons la variance pour cet ensemble de données étape par étape :")
+
+
+     st.markdown("")
+
+
+     st.markdown("Étape 1 : Calculons la Moyenne (**μ**) des scores de satisfaction")
+     st.latex(r'Variance (\sigma^2) = \frac{\sum_{i=1}^{n}(X_i - \mu)^2}{N}')
+
     
     
 
