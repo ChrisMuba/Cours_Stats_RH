5984a37e1ef9351030a6bc2fae53e7c4bfd77cd1
ChrisMuba <63189970+ChrisMuba@users.noreply.github.com>
2023-09-16 18:25:26 +0200

    Update 2_Chapitre_2_🔖_Statistiques_descriptives.py

--- pages/2_Chapitre_2_🔖_Statistiques_descriptives.py
+++ pages/2_Chapitre_2_🔖_Statistiques_descriptives.py
@@ -761,7 +761,7 @@ if st.button("Continuer vers la suite du Chap.2 - **C/ Techniques graphiques**")
 
 # Create a Plotly histogram figure with a custom legend label and no legend title
     fig = px.histogram(salaries, nbins=20, title='Répartition des salaires dans notre entreprise')
-    fig.update_xaxes(title_text='Salaire (en milliers de d\'euros)')
+    fig.update_xaxes(title_text='Salaire (en milliers de d\'€)')
     fig.update_yaxes(title_text='Nombre d\'employés')
 
 # Remove the legend title
@@ -773,7 +773,7 @@ if st.button("Continuer vers la suite du Chap.2 - **C/ Techniques graphiques**")
 
     st.markdown("**🔮Interpretation de l'histogramme**")
 
-    st.markdown("Cet histogramme est une représentation graphique de la la répartition des salaires entre les salariés de notre entreprise. Chaque barre de l'histogramme correspond à une échelle salariale spécifique (en milliers d'euros), et la hauteur de chaque barre indique le nombre d'employés dans cette fourchette salariale.")
+    st.markdown("Cet histogramme est une représentation graphique de la la répartition des salaires entre les salariés de notre entreprise. Chaque barre de l'histogramme correspond à une échelle salariale spécifique (en milliers d'€), et la hauteur de chaque barre indique le nombre d'employés dans cette fourchette salariale.")
     st.markdown("Échelles salariales courantes : les barres les plus hautes de l'histogramme se situent dans les échelles salariales [45 k€ - 49,99 k€] et [50 k€ - 54,99 k€]. Cela suggère que ces échelles salariales sont les plus courantes parmi nos employés, avec respectivement 23 et 22 employés.")
     st.markdown("Répartition des salaires : l'histogramme montre que les salaires sont répartis sur une plage de valeurs relativement uniforme sans écarts ni regroupements significatifs.")
     st.markdown("Asymétrie : l'histogramme semble être légèrement incliné vers la droite. Cela signifie qu'il y a relativement moins d'employés dans les échelles salariales inférieures ([20 k€ - 24,99 k€] à [40 k€ - 44,99 k€]) et plus d'employés dans les échelles salariales moyennes à supérieures ([45 k€ - 49,99 k€] à [65 k€ - 69.99 k€].")
