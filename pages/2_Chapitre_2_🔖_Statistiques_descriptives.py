05fd2f8b358835c5bea0ac1bc2af75a3e57d1e97
ChrisMuba <63189970+ChrisMuba@users.noreply.github.com>
2023-09-17 19:41:17 +0200

    Update 2_Chapitre_2_🔖_Statistiques_descriptives.py

--- pages/2_Chapitre_2_🔖_Statistiques_descriptives.py
+++ pages/2_Chapitre_2_🔖_Statistiques_descriptives.py
@@ -1029,6 +1029,39 @@ if st.button("Continuer vers la suite du Chap.2 - **C/ Techniques graphiques**")
 
 
 
+    import streamlit as st
+    import plotly.express as px
+
+# Create the Streamlit app
+    st.title('Employee Absences and Vacations Heatmap')
+
+# Create filters for department and month
+    selected_department = st.selectbox('Select Department:', ['HR', 'Sales', 'IT'])
+    selected_month = st.selectbox('Select Month:', ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'])
+
+# Filter the data based on user selections
+    filtered_data = employee_data[(employee_data['Department'] == selected_department) & (employee_data['Date'].dt.strftime('%B') == selected_month)]
+
+# Pivot the filtered data for plotting
+    heatmap_data = filtered_data.pivot(index='Department', columns='Date', values='Absences')
+
+# Create an interactive heatmap using Plotly Express
+    fig = px.imshow(
+        heatmap_data,
+        x=heatmap_data.columns,
+        y=heatmap_data.index,
+        color_continuous_scale='Viridis',  # You can choose different color scales
+        title=f'Employee Absences Heatmap for {selected_department} in {selected_month}'
+    )
+    fig.update_xaxes(title_text='Date')
+    fig.update_yaxes(title_text='Department')
+
+# Show the plot in Streamlit
+    st.plotly_chart(fig)
+
+
+
+
 
     
 
