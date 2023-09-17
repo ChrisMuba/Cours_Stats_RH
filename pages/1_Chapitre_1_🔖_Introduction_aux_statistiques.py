496231302e5a09ee6f32c5e65a11a04c289e7e3f
ChrisMuba <63189970+ChrisMuba@users.noreply.github.com>
2023-09-18 01:59:32 +0200

    Update 1_Chapitre_1_🔖_Introduction_aux_statistiques.py

--- pages/1_Chapitre_1_🔖_Introduction_aux_statistiques.py
+++ pages/1_Chapitre_1_🔖_Introduction_aux_statistiques.py
@@ -89,9 +89,9 @@ if st.button("Continuer vers la suite du Chap.1 - **B/ Types de données et sour
 
 # Sample HR dataset
     data = {
-        'Employee ID': ['001', '002', '003', '004', '005', '006'
-        'Service': ['Ventes', 'Finance', 'RH', 'IT', 'Marketing', 'Operations'
-        'Genre': ['Femme', 'Femme', 'Homme', 'Femme', 'Homme', 'Homme'
+        'Employee ID': ['001', '002', '003', '004', '005', '006']
+        'Service': ['Ventes', 'Finance', 'RH', 'IT', 'Marketing', 'Operations']
+        'Genre': ['Femme', 'Femme', 'Homme', 'Femme', 'Homme', 'Homme']
     }
 
     df = pd.DataFrame(data)
