568b313351a0747eda21ea7b8be3fa844d33278f
ChrisMuba <63189970+ChrisMuba@users.noreply.github.com>
2023-09-18 12:54:26 +0200

    Update 2_Heatmap_en_bonus.py

--- pages/2_Heatmap_en_bonus.py
+++ pages/2_Heatmap_en_bonus.py
@@ -1,52 +1,52 @@
 
 
-    import pandas as pd
-    import numpy as np
+import pandas as pd
+import numpy as np
 
 # Create sample data for employee absences and vacations
-    np.random.seed(42)
+np.random.seed(42)
 
 # Generate random data for three departments: HR, Sales, and IT
-    departments = ['HR', 'Sales', 'IT']
-    dates = pd.date_range(start='2022-01-01', end='2022-12-31', freq='D')
-    data = []
+departments = ['HR', 'Sales', 'IT']
+dates = pd.date_range(start='2022-01-01', end='2022-12-31', freq='D')
+data = []
 
-    for date in dates:
-        for department in departments:
-            absences = np.random.randint(0, 5)  # Random daily absences (0 to 4)
-            vacations = np.random.randint(0, 3)  # Random daily vacations (0 to 2)
-            data.append([date, department, absences, vacations])
+for date in dates:
+    for department in departments:
+        absences = np.random.randint(0, 5)  # Random daily absences (0 to 4)
+        vacations = np.random.randint(0, 3)  # Random daily vacations (0 to 2)
+        data.append([date, department, absences, vacations])
                 
 
-    import streamlit as st
-    import plotly.express as px
+import streamlit as st
+import plotly.express as px
 
 # Create the Streamlit app
-    st.title('Employee Absences and Vacations Heatmap')
+st.title('Employee Absences and Vacations Heatmap')
 
 # Create filters for department and month
-    selected_department = st.selectbox('Select Department:', ['HR', 'Sales', 'IT'])
-    selected_month = st.selectbox('Select Month:', ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'])
+selected_department = st.selectbox('Select Department:', ['HR', 'Sales', 'IT'])
+selected_month = st.selectbox('Select Month:', ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'])
 
 # Filter the data based on user selections
-    filtered_data = employee_data[(employee_data['Department'] == selected_department) & (employee_data['Date'].dt.strftime('%B') == selected_month)]
+filtered_data = employee_data[(employee_data['Department'] == selected_department) & (employee_data['Date'].dt.strftime('%B') == selected_month)]
 
 # Pivot the filtered data for plotting
-    heatmap_data = filtered_data.pivot(index='Department', columns='Date', values='Absences')
+heatmap_data = filtered_data.pivot(index='Department', columns='Date', values='Absences')
 
 # Create an interactive heatmap using Plotly Express
-    fig = px.imshow(
-        heatmap_data,
-        x=heatmap_data.columns,
-        y=heatmap_data.index,
-        color_continuous_scale='Viridis',  # You can choose different color scales
-        title=f'Employee Absences Heatmap for {selected_department} in {selected_month}'
-    )
-    fig.update_xaxes(title_text='Date')
-    fig.update_yaxes(title_text='Department')
+fig = px.imshow(
+    heatmap_data,
+    x=heatmap_data.columns,
+    y=heatmap_data.index,
+    color_continuous_scale='Viridis',  # You can choose different color scales
+    title=f'Employee Absences Heatmap for {selected_department} in {selected_month}'
+)
+fig.update_xaxes(title_text='Date')
+fig.update_yaxes(title_text='Department')
 
 # Show the plot in Streamlit
-    st.plotly_chart(fig)
+st.plotly_chart(fig)
 
 
 
