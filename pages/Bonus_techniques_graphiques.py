8918485fdb3e6685890ca9aaab75b2d468b4a7ee
ChrisMuba <63189970+ChrisMuba@users.noreply.github.com>
2023-09-18 12:37:25 +0200

    Add files via upload

---
+++ pages/Bonus_techniques_graphiques.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import streamlit as st
+
+st.title("Cours de Statistiques appliquées aux Ressources Humaines")
+
+st.header("Auteur : Christian MUBA - HR Data analyst.")
+
+st.caption("Vous souhaitez faire analyser vos data RH, prendre des décisions éclairées ou bénéficier d'un tutorat personnalisé ? contactez-moi 📧https://www.linkedin.com/in/chris-muba-io 🌍")
+
+# st.markdown("Ce projet porte sur l'étude d'un jeu de données RH comportant 13 colonnes & 8336 lignes")
+
+# st.caption("Fichier de données avant nettoyage : www.example.com")
+
+st.subheader("Description du cours")
+
+st.markdown("Ce cours offre une introduction aux statistiques pour les professionnels des ressources humaines. L'accent est mis sur la compréhension de la façon d'analyser et d'interpréter les données statistiques pour prendre des décisions éclairées. Les sujets couverts comprennent la collecte de données, les mesures de la tendance centrale et de la variabilité, la probabilité, les tests d'hypothèses, la corrélation et la régression.")
+
+st.subheader("🚀Objectifs d'apprentissage🚀")
+
+st.markdown("A la fin de ce cours, les étudiants seront capables de :")
+
+st.markdown("🎯Comprendre comment les données statistiques peuvent éclairer les décisions liées aux RH")
+st.markdown("🎯Recueillir, résumer et analyser des données à l'aide de statistiques descriptives")
+st.markdown("🎯Interpréter les données à l'aide de techniques statistiques inférentielles")
+st.markdown("🎯Évaluer la validité des conclusions statistiques basées sur des données d'échantillon")
+st.markdown("🎯Appliquer des techniques statistiques aux problèmes liés aux RH")
+
+
+st.markdown("")
+
+
+def redirect_button(url: str, text: str= None, color="#FD504D"):
+        st.markdown(
+        f"""
+        <a href="{url}" target="_blank">
+            <div style="
+                display: inline-block;
+                padding: 0.5em 1em;
+                color: #FFFFFF;
+                background-color: {color};
+                border-radius: 3px;
+                text-decoration: none;">
+                {text}
+            </div
+        </a>
+        """,
+        unsafe_allow_html=True
+        )
+redirect_button("https://cours-stats-rh.streamlit.app/Chapitre_1_🔖_Introduction_aux_statistiques","Aller au chapitre 1")
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
