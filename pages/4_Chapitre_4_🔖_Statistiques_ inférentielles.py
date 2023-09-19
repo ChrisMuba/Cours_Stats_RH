87872b416afe0e642fb32c936a127a9cb7f08c81
ChrisMuba <63189970+ChrisMuba@users.noreply.github.com>
2023-09-19 18:51:38 +0200

    Update 4_Chapitre_4_🔖_Statistiques_ inférentielles.py

--- pages/4_Chapitre_4_🔖_Statistiques_ inférentielles.py
+++ pages/4_Chapitre_4_🔖_Statistiques_ inférentielles.py
@@ -49,13 +49,13 @@ if st.button("Cliquez pour acceder au Chap.4 - **A/ Estimation**"):
     st.markdown("Le *paramètre* à estimer peut-être par exemple, la moyenne, la proportion, etc...")
     
     st.markdown("- **L'estimation ponctuelle** consiste à estimer une valeur unique pour un paramètre de population sur la base d'un échantillon de données.") 
-    st.markdown("Par exemple, on peut utiliser la moyenne de l’échantillon des scores de satisfaction des employés pour estimer le niveau de satisfaction global au sein de l’entreprise.")
+    st.markdown("")
     
-    st.markdown("- **Exemple 4 - Application possible en RH** : Supposons que nous sélectionnions au hasard 100 employés d'une grande entreprise et constations que leur salaire moyen est de 50 000 €. Nous pouvons utiliser cette moyenne d'échantillon comme estimation ponctuelle du salaire moyen de la population.")
+    st.markdown("- 🏀**Application 14** : Par exemple, on peut utiliser la moyenne de l’échantillon des scores de satisfaction des employés pour estimer le niveau de satisfaction global au sein de l’entreprise.")
     
     st.markdown("- **L'estimation par intervalle**, d'autre part, fournit une gamme de valeurs plausibles pour le paramètre de population. Cette plage est appelée **intervalle de confiance**. **Les intervalles de confiance** nous **aident à comprendre l'incertitude associée à notre estimation**.")
                 
-    st.markdown("- **Exemple 4 - Application possible en RH** : En utilisant le même exemple que ci-dessus, si nous calculons un intervalle de confiance à 95 % pour le salaire moyen et que nous trouvons qu'il est de 48 000 € à 52 000 €, cela signifie que nous sommes sûrs à 95 % que le salaire moyen réel de la population se situe dans cette fourchette.")           
+    st.markdown("- 🏀**Application 15** : En utilisant le même exemple que ci-dessus, si nous calculons un intervalle de confiance à 95 % pour le salaire moyen et que nous trouvons qu'il est de 48 000 € à 52 000 €, cela signifie que nous sommes sûrs à 95 % que le salaire moyen réel de la population se situe dans cette fourchette.")           
 
 
 if st.button("Continuer vers la suite du Chap.4 - **B/ Tests d'hypothèses**"):
@@ -80,7 +80,7 @@ if st.button("Continuer vers la suite du Chap.4 - **B/ Tests d'hypothèses**"):
     
     st.markdown("f. **Tirer des conclusions basées sur la décision et interpréter les résultats**")
     
-    st.markdown("- **Exemple 4 - Application possible en RH** : Supposons qu'un contrôleur de gestion sociale souhaite déterminer s'il existe un écart de rémunération entre les 👦🏾 et 👧 dans l'entreprise (l'écart de rémunération fait partie des indicateurs qui composent **l’index de l’égalité femmes-hommes**).") 
+    st.markdown("- 🏀**Application 16** : Supposons qu'un contrôleur de gestion sociale souhaite déterminer s'il existe un écart de rémunération entre les 👦🏾 et 👧 dans l'entreprise (l'écart de rémunération fait partie des indicateurs qui composent **l’index de l’égalité femmes-hommes**).") 
     
     st.markdown("- **L'hypothèse nulle (H0)** serait : **Il n'y a pas de différence significative dans les salaires moyens** entre les 👦🏾 et 👧") 
     
@@ -92,17 +92,17 @@ if st.button("Continuer vers la suite du Chap.4 - **C/ Tests statistiques commun
     
     st.markdown("- **Test t : il est utilisé pour comparer les moyennes entre deux groupes**. En RH, il peut être utilisé pour évaluer s'il existe une différence significative dans les salaires moyens entre les différents postes, services ou effectifs 👦🏾 et 👧.")
 
-    st.markdown("- **Exemple 4 - Application possible en RH** : Un contrôleur de gestion sociale peut aussi effectuer un **test t** pour déterminer s'il existe une différence significative dans les notes de performance moyennes entre les employés qui ont suivi un programme de formation interne et ceux qui ne l'ont pas fait.")
+    st.markdown("- 🏀**Application 17** : Un contrôleur de gestion sociale peut aussi effectuer un **test t** pour déterminer s'il existe une différence significative dans les notes de performance moyennes entre les employés qui ont suivi un programme de formation interne et ceux qui ne l'ont pas fait.")
     
     st.markdown("- **Test du chi carré (Test du χ²) : ce test est utilisé pour déterminer s'il existe une association entre deux variables catégorielles**.") 
     
-    st.markdown("- **Exemple 4 - Application possible en RH** : Un contrôleur de gestion sociale peut utiliser le **test du χ²** pour examiner s'il existe une relation significative entre les niveaux de satisfaction des employés (variable catégorielle) et leur engagement dans des programmes de développement professionnel (une autre variable catégorielle).") 
+    st.markdown("- 🏀**Application 18** : Un contrôleur de gestion sociale peut utiliser le **test du χ²** pour examiner s'il existe une relation significative entre les niveaux de satisfaction des employés (variable catégorielle) et leur engagement dans des programmes de développement professionnel (une autre variable catégorielle).") 
     
     st.markdown("Grâce au résultat du test, le contrôleur de gestion sociale pourra aider les autres professionnels RH à comprendre s'il existe un lien entre ces variables, et à décider si des interventions ciblées sont nécessaires pour améliorer la satisfaction des employés.")
     
     st.markdown("- **ANOVA (Analyse de la Variance): L'ANOVA est utilisée pour comparer les moyennes entre trois groupes ou plus**. En RH, elle peut être utilisée pour analyser s'il existe des différences significatives dans la comparaison de l'absentéisme moyen entre les employés de différents services, à différentes catégories de postes, ou à différents niveaux d'expérience.")
     
-    st.markdown("- **Exemple 4 - Application possible en RH** : Un contrôleur de gestion sociale peut utiliser **l'ANOVA** pour rechercher s'il existe des variations significatives dans les heures d'absences des employés entre les différents groupes d'âge (par exemple, 20-30, 31-40, 41-50, 51+). Cette analyse peut donner un aperçu de la nécessité de stratégies ciblées pour améliorer l'engagement et le climat social en fonction des différentes tranches d'âge.")
+    st.markdown("- 🏀**Application 19** : Un contrôleur de gestion sociale peut utiliser **l'ANOVA** pour rechercher s'il existe des variations significatives dans les heures d'absences des employés entre les différents groupes d'âge (par exemple, 20-30, 31-40, 41-50, 51+). Cette analyse peut donner un aperçu de la nécessité de stratégies ciblées pour améliorer l'engagement et le climat social en fonction des différentes tranches d'âge.")
     
     st.markdown("")
 
