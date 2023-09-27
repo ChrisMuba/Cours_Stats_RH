2f7555da2921f5981dfca2572ea4e84fc20c268b
ChrisMuba <63189970+ChrisMuba@users.noreply.github.com>
2023-09-27 11:48:25 +0200

    Update 5_Chapitre_5_🔖_Correlation_et_Regression.py

--- pages/5_Chapitre_5_🔖_Correlation_et_Regression.py
+++ pages/5_Chapitre_5_🔖_Correlation_et_Regression.py
@@ -288,7 +288,7 @@ if st.button("Continuer vers la suite du Chap.5 - **B/ Régression linéaire sim
     st.write("🔮Interpretation:")
     st.write("Une régression linéaire simple nous aide à comprendre la relation entre les années d'expérience et l'évaluation des performances.")
     st.write(f"L'équation de régression est: Notes de performance = {model.intercept_:.2f} + {model.coef_[0]:.2f} * Années d'expérience")
-    st.write("Ici, l'interception représente la note de performance attendue lorsqu'un employé a 0 année d'expérience.")
+    st.write("Ici, l'intercept représente la note de performance attendue lorsqu'un employé a 0 année d'expérience.")
     st.write("Le coefficient représente la variation attendue de la note de performance pour une augmentation d'une unité du nombre d'années d'expérience.")
     st.write("Dans notre cas, pour chaque année d'expérience supplémentaire, la note de performance attendue augmente d'environ 0,50 point.")
 
