e5f6aa9d2450827d05637cf62c03d7fb6956c017
ChrisMuba <63189970+ChrisMuba@users.noreply.github.com>
2023-09-29 00:38:54 +0200

    Update 6_Bibliographie.py

--- pages/6_Bibliographie.py
+++ pages/6_Bibliographie.py
@@ -3,6 +3,6 @@ import streamlit as st
 
 st.title('Bibliographie')
 
-st.markdown("Les statistiques : le pouvoir des données! https://www150.statcan.gc.ca/n1/edu/power-pouvoir/toc-tdm/5214718-fra.htm")
+st.markdown("Les statistiques : le pouvoir des données ! https://www150.statcan.gc.ca/n1/edu/power-pouvoir/toc-tdm/5214718-fra.htm")
 
 st.title('_Streamlit_ is :blue[cool] :sunglasses:')
