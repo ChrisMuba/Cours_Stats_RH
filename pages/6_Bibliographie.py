04053dfbe080074e95e8f5667a8eff7156715a33
ChrisMuba <63189970+ChrisMuba@users.noreply.github.com>
2023-09-29 00:49:40 +0200

    Update 6_Bibliographie.py

--- pages/6_Bibliographie.py
+++ pages/6_Bibliographie.py
@@ -3,9 +3,9 @@ import streamlit as st
 
 st.title('Bibliographie')
 
-st.markdown("https://streamlit.io")
+st.markdown("*A faster way to build and share data apps* https://streamlit.io")
 
-st.markdown("https://plotly.com")
+st.markdown("*Low-Code Data Apps* https://plotly.com")
 
 st.markdown("Les statistiques : le pouvoir des données ! https://www150.statcan.gc.ca/n1/edu/power-pouvoir/toc-tdm/5214718-fra.htm")
 
