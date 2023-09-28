a5a619e4638e9f88a6bb3f37e296e4c7b061a2bf
ChrisMuba <63189970+ChrisMuba@users.noreply.github.com>
2023-09-29 00:59:11 +0200

    Update 6_Bibliographie.py

--- pages/6_Bibliographie.py
+++ pages/6_Bibliographie.py
@@ -7,6 +7,10 @@ st.markdown("*A faster way to build and share data apps* https://streamlit.io")
 
 st.markdown("*Low-Code Data Apps* https://plotly.com")
 
+st.markdown("*Python Data Analysis Library* https://pandas.pydata.org)
+
+st.markdown("*The fundamental package for scientific computing with Python* https://numpy.org)            
+
 st.markdown("Les statistiques : le pouvoir des données ! https://www150.statcan.gc.ca/n1/edu/power-pouvoir/toc-tdm/5214718-fra.htm")
 
 st.markdown("")
