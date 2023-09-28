57abe35705208dc18a83d2d3cdd67b232a960b4b
ChrisMuba <63189970+ChrisMuba@users.noreply.github.com>
2023-09-29 01:05:30 +0200

    Update 6_Bibliographie.py

--- pages/6_Bibliographie.py
+++ pages/6_Bibliographie.py
@@ -13,7 +13,7 @@ st.markdown("*Python Data Analysis Library* https://pandas.pydata.org")
 
 st.markdown("*The fundamental package for scientific computing with Python* https://numpy.org")            
 
-st.markdown("Les statistiques : le pouvoir des données ! https://www150.statcan.gc.ca/n1/edu/power-pouvoir/toc-tdm/5214718-fra.htm")
+st.markdown("*Les statistiques : le pouvoir des données !* https://www150.statcan.gc.ca/n1/edu/power-pouvoir/toc-tdm/5214718-fra.htm")
 
 st.markdown("")
 
