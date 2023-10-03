d1365d0f35b51f5100df307620ce515b56390fc2
ChrisMuba <63189970+ChrisMuba@users.noreply.github.com>
2023-10-04 01:43:27 +0200

    Update 🏠_Syllabus_du_cours.py

--- 🏠_Syllabus_du_cours.py
+++ 🏠_Syllabus_du_cours.py
@@ -33,7 +33,7 @@ st.markdown("🎯Évaluer la validité des conclusions statistiques basées sur 
 st.markdown("🎯Appliquer des techniques statistiques aux problèmes liés aux RH")
 
 with st.sidebar:
-    st.image('GIF_loading_data.gif')
+    st.image('GIF/GIF_loading_data.gif')
 
 
 st.markdown("")
